af5885e0a965ce65637728c888390f4749280b04
JinTak <jtak23@gmail.com>
2018-01-03 16:37:05 -0700

    made changes to server.js homepage

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
+    woops_i_has_forgot_to_document_all_my_endpoints: false, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentation_url: "https://github.com/JinTak/express-personal-api/blob/master/README.md", // CHANGE ME
+    base_url: "https://afternoon-inlet-66263.herokuapp.com/", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
@@ -49,6 +49,8 @@ app.get('/api', function api_index(req, res) {
     ]
   })
 });
+
+
 
 /**********
  * SERVER *
